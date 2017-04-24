afd72707cd7cb9f7bd6689c6082ff7b45059ad27
Roman Belov <3cL1p5e7@gmail.com>
2017-04-24 16:15:42 +0300

    done

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+var path = require('path');
 module.exports = function (content) {
   this.cacheable();
+
+  var query = this.query.substr(1);
 
   var code = content.split('<style>', 2)[1];
   var result = content;
   if (code) {
     var source = code.split('</style>', 2);
-    var preamble = 'require(' + JSON.stringify(require.resolve('style-loader/addStyles')) + ')(' +
-      JSON.stringify([[this.resourcePath, source[0]]]) + ');';
-    result = preamble + code[0] + source[1];
+    var preamble = '';
+    if (query === 'true') {
+      result = source[0];
+    }
+    else {
+      var style = `require("!!css-loader?sourceMap!sass-loader!stylextract-loader?true!./${path.basename(this.resourcePath)}")`;
+      preamble = `require(${JSON.stringify(require.resolve('style-loader/addStyles'))})([["${this.resourcePath}", ${style}]]);`;
+      result = preamble + code[0] + source[1];
+    }
   }
   return result;
 };
